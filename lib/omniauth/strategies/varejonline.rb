1134e46e60a1ccea3ecc69d78f2918c748baa8a4
Gustavo Berdugo <gberdugo@gmail.com>
2015-11-25 15:20:20 -0200

    Alteração do endereço da API

--- lib/omniauth/strategies/varejonline.rb
+++ lib/omniauth/strategies/varejonline.rb
@@ -8,21 +8,19 @@ module OmniAuth
       DEFAULT_SCOPE = "all"
 
       option :name, 'varejonline'
-      option :authorize_options, [:app_id, :redirect_uri]
-      option :provider_ignores_state, :true
+      option :authorize_options, [:client_id, :redirect_uri, :client_secret]
+      option :provider_ignores_state, true
 
       option :client_options, {
-        :site          => 'https://integrador.varejonline.com.br',
+        :site          => 'https://erp.varejonline.com.br',
         :authorize_url => '/apps/oauth/authorization?response_type=code',
         :token_url     => '/apps/oauth/token'
       }
 
-      option :token_params, {
-        :formato => :json
-      }
-
-      def request_phase
-        redirect client.auth_code.authorize_url({:redirect_uri => options.redirect_uri }.merge(options.authorize_params)).gsub("client_id", "app_id")
+      def build_access_token
+        verifier = request.params["code"]
+        
+        client.auth_code.get_token(verifier, {:redirect_uri => callback_url.gsub(/\?(.*)/, "")}.merge(token_params.to_hash(:symbolize_keys => true)), deep_symbolize(options.auth_token_params))
       end
 
       info do
@@ -44,44 +42,6 @@ module OmniAuth
           prune!(value) if value.is_a?(Hash)
           value.nil? || (value.respond_to?(:empty?) && value.empty?)
         end
-      end
-    end
-  end
-end
-
-module OAuth2
-  class Client
-    def get_token(params, access_token_opts={})
-      opts = {:raise_errors => options[:raise_errors], :parse => params.delete(:parse)}
-      
-      if options[:token_method] == :post
-        headers = params.delete(:headers)
-
-        if params[:formato] == :json
-          params[:redirect_uri] = params[:redirect_uri].gsub(/\?(.*)/, "")
-
-          opts[:body] = params.to_json
-          opts[:headers] =  {'Content-Type' => 'application/json'}
-        else
-          opts[:body] = params
-          opts[:headers] =  {'Content-Type' => 'application/x-www-form-urlencoded'}
-        end
-
-        opts[:headers].merge!(headers) if headers
-      else
-        opts[:params] = params
-      end
-
-      response = request(options[:token_method], token_url, opts)
-      raise Error.new(response) if options[:raise_errors] && !(response.parsed.is_a?(Hash) && response.parsed['access_token'])
-      AccessToken.from_hash(self, response.parsed.merge(access_token_opts))
-    end
-  end
-
-  module Strategy
-    class Base
-      def client_params
-        {'app_id' => @client.id, 'app_secret' => @client.secret, 'client_id' => @client.id, 'client_secret' => @client.secret}
       end
     end
   end
